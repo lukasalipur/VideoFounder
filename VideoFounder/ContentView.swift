0f510af9df47f9d37bb35973818800f8476915c9
Luka <lukasalipur.dev@gmail.com>
2022-08-07 01:49:40 +0200

    Initial Commit

---
+++ VideoFounder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoFounder
+//
+//  Created by Luka Šalipur on 7.8.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoFounder/VideoFounderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFounderApp.swift
+//  VideoFounder
+//
+//  Created by Luka Šalipur on 7.8.22..
+//
+
+import SwiftUI
+
+@main
+struct VideoFounderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
